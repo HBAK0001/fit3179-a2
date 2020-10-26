28d50fea05a9d8148332dc8886a54a7c28b393ee
Hayden Baker <hbak0001@student.monash.edu>
2020-10-26 13:04:10 +1100

    Added data for all candidate first preferences

--- js/fed-js.js
+++ js/fed-js.js
@@ -1,47 +1,24 @@
-function changeMap(type, state, subgroup) {
-    var i;
-    var x = document.getElementsByClassName(state+"-map");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(state+'-'+subgroup).style.display = "block";
-}
+/*
+    Display Main Map Visualisation
+*/
+var spec_vic = "https://HBAK0001.github.io/fit3179-a2/fed.nat.2019.chart.json";
+            
+            vegaEmbed('#vis-main', spec_vic).then(function(result) {
+                result.view.addEventListener('click', function(event, item){
+                    displayElectorateInfo(item);
+                });
+            }).catch(console.error);
 
 /*
- *  Vega embedding
- */
-
-var spec_vic = "https://HBAK0001.github.io/fit3179-a2/fed.vic.chart.json";
-vegaEmbed('#vis-vic', spec_vic).then(function(result) {
-}).catch(console.error);
-var spec_vic_metro = "https://HBAK0001.github.io/fit3179-a2/fed.vic.chart.metro.json";
-vegaEmbed('#vis-vic-metro', spec_vic_metro).then(function(result) {
-}).catch(console.error);
-
-var spec_nsw = "https://HBAK0001.github.io/fit3179-a2/fed.nsw.chart.json";
-vegaEmbed('#vis-nsw', spec_nsw).then(function(result) {
-}).catch(console.error);
-var spec_wa = "https://HBAK0001.github.io/fit3179-a2/fed.wa.chart.json";
-vegaEmbed('#vis-wa', spec_wa).then(function(result) {
-}).catch(console.error);
-var spec_qld = "https://HBAK0001.github.io/fit3179-a2/fed.qld.chart.json";
-vegaEmbed('#vis-qld', spec_qld).then(function(result) {
-}).catch(console.error);
-
-var spec_sa = "https://HBAK0001.github.io/fit3179-a2/fed.sa.chart.json";
-vegaEmbed('#vis-sa', spec_sa).then(function(result) {
-}).catch(console.error);
-var spec_tas = "https://HBAK0001.github.io/fit3179-a2/fed.tas.chart.json";
-vegaEmbed('#vis-tas', spec_tas).then(function(result) {
-}).catch(console.error);
-var spec_nt = "https://HBAK0001.github.io/fit3179-a2/fed.nt.chart.json";
-vegaEmbed('#vis-nt', spec_nt).then(function(result) {
-}).catch(console.error);
-var spec_act = "https://HBAK0001.github.io/fit3179-a2/fed.act.chart.json";
-vegaEmbed('#vis-act', spec_act).then(function(result) {
-}).catch(console.error);
-
-const states = ["vic", "nsw"]
-states.forEach(function (item, index) {
-    changeMap("fed", item, "regional")
-})
+    Display electorate results when clicked
+*/
+function displayElectorateInfo(item) {
+    if (item !== null){
+        let division = item.datum.properties.Sortname;
+        console.log(division);
+    }
+    else {
+        let division = "national";
+        console.log(division);
+    }
+}
